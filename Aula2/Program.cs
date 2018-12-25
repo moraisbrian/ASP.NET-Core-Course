43e6f1e26876462fa16f1707ff0c1f18986fc14d
Brian Leonardo de Morais <morais.brian360@gmail.com>
2018-12-25 10:05:43 -0200

    Seção 2, aula 10 concluída

--- Aula2/Program.cs
+++ Aula2/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 
 namespace aula2
 {
@@ -6,7 +9,25 @@ namespace aula2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var host = new WebHostBuilder()
+            .UseKestrel()
+            // .Configure(app => {
+            //     app.Run(context => context.Response.WriteAsync("Bem vindo!"));
+            // })
+            .UseStartup<Startup>()
+            .Build();
+
+            host.Run();
+        }
+    }
+
+    internal class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Use(async(context, next)=>{
+                await context.Response.WriteAsync("Trabalhando com classe StartUp");
+            });
         }
     }
 }
