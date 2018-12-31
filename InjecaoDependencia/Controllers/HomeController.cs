6de9b2d6b745aac0c6e883b3dea44e1b3441eb4d
Brian Leonardo de Morais <morais.brian360@gmail.com>
2018-12-31 17:34:58 -0200

    Aula 19 concluida

---
+++ InjecaoDependencia/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using injecaodependencia.Models;
+using injecaodependencia.Repository;
+
+namespace injecaodependencia.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IPeopleRepository _pepleRepository;
+        public HomeController(IPeopleRepository repository)
+        {
+            _pepleRepository = repository;
+        }
+        public IActionResult Index()
+        {
+            ViewData["name"] = _pepleRepository.GetNameById(2);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
