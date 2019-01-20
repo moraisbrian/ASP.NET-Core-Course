ebb181feccc972a5e55c69f710f1e128b6d2b2d6
Brian Leonardo de Morais <morais.brian360@gmail.com>
2019-01-20 19:25:07 -0200

    Aula 40 concluida

--- EFCore/Dominio/Entidades/Produto.cs
+++ EFCore/Dominio/Entidades/Produto.cs
@@ -9,5 +9,6 @@ namespace Dominio.Entidades
         public int Id { get; set; }
         public string Nome { get; set; }
         public Categoria Categoria { get; set; }
+        public int CategoriaId { get; set; }
     }
 }
---
+++ EFCore/Mvc/Controllers/ProdutoController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dados;
+using Dominio.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mvc.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProdutoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Salvar()
+        {
+            ViewBag.Categorias = _context.Categorias.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Salvar(Produto produto)
+        {
+            //var categoria = _context.Categorias.First(x => x.Id == produto.CategoriaId);
+            //produto.Categoria = categoria;
+            _context.Produtos.Add(produto);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Consultar");
+        }
+
+        [HttpGet]
+        public IActionResult Consultar()
+        {
+            var produto = _context.Produtos.ToList();
+            return View(produto);
+        }
+    }
+}
