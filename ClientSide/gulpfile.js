aa74df759060d00d80bbc8bdd16f626e33709591
Brian Leonardo de Morais <morais.brian360@gmail.com>
2019-02-10 12:51:01 -0200

    Aula 67 concluida

---
+++ ClientSide/gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var cssmin = require('gulp-cssmin');
+var uncss = require('gulp-uncss')
+
+gulp.task('js', function () {
+    return gulp.src([
+        './node_modules/bootstrap/dist/js/bootstrap.min.js',
+        './node_modules/jquery/dist/jquery.min.js'
+    ])
+    .pipe(gulp.dest('./wwwroot/js'));
+});
+
+gulp.task('css', function () {
+    return gulp.src([
+        './Styles/site.css',
+        './node_modules/bootstrap/dist/css/bootstrap.css'
+    ])
+    .pipe(concat('site.min.css'))
+    .pipe(cssmin())
+    .pipe(uncss({html: ['Views/**/*.cshtml']}))
+    .pipe(gulp.dest('./wwwroot/css'));
+});
+
+gulp.task('watch-css', function(){
+    gulp.watch('./Styles/**/*.css', ['css']);
+});
