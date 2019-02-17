cad30527db223893b159a7973876535a20bf0307
Brian Leonardo de Morais <morais.brian360@gmail.com>
2019-02-17 11:05:44 -0300

    Curso concluido

---
+++ Aplicacao/src/StoreOfBuild.Web/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StoreOfBuild.Web.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace StoreOfBuild.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateOrEdit()
+        {
+            return View();
+        }
+
+        [httpPost]
+        public IActionResult CreateOrEdit(int id)
+        {
+            return View();
+        }
+
+    }
+}
